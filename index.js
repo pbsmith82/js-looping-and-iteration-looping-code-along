f15168532af03c8a7f1454c676c11ccce3f21aa1
pbsmith82 <pbsmith82@gmail.com>
2021-03-14 14:06:08 -0400

    commit tree 85776174285de359de11cf5f213ef55a397078b0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let array2 =[]
+    for (let i = 0; i < array.length; i++) {
+    array2.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return array2
+
+}
+
+function countDown(number) {
+    let number2 = 0
+    while (number2 < (number + 1) ) {
+      let number3 = number - number2
+      console.log(number3)
+      number2++
+    }
+
+}
